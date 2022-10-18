9cf3781b826d2f673addf057171f744585a820ae
shoba-001 <jakkulashoba@gmail.com>
2022-10-18 12:13:21 +0530

    New Proj4

--- Condition1.java
+++ Condition1.java
@@ -3,13 +3,14 @@ package class4;
 public class Condition1 {
 	public static void main(String args[]) {
 		int age=17;
+		String name="Manu";
 		if(age>=18) {
 			System.out.println("You are eligible to vote");
 		}
 		else {
 			System.out.println("You are not eligible to vote");
 		}
-		
+		System.out.println(name);
 		}
 	}
 
